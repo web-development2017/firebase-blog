2283a1195265a5b372186c30bf0fb733fbfb5272
Rich <web_dev2017@outlook.com>
2022-04-10 16:35:13 +0100

    Build failed as had BrowserRouter in code twice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-      </Routes>
-    </Router>
+    
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+    </Routes>
+    
   );
 }
 
